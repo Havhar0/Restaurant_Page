c2fe1cc2fa2bba8e577e26374a5cc9682b35b538
Havhar0 <112910346+Havhar0@users.noreply.github.com>
2023-02-09 14:37:50 +0000

    Menu page fixed

---
+++ src/functions/menuLoad.js
@@ -0,0 +1,10 @@
+import menu from  "../pages/menuPage.js";    
+
+const menuLoad = function() {
+    // const content = document.querySelector('#content');
+    // const menuPage = menu();
+    // content.append(menu);
+    menu()
+}
+
+export default menuLoad;
